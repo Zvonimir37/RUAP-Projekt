7742cd2484a17ea4c5a6313b46aa7426da95867b
Zvonimir37 <zvonac50@hotmail.com>
2020-12-04 17:59:00 +0100

    Added change in VS file, name printout

--- LV_ConsoleAppZad1/Program.cs
+++ LV_ConsoleAppZad1/Program.cs
@@ -6,7 +6,7 @@ namespace LV_ConsoleAppZad1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pozdrav iz aplikacije od Zvonimira Hajdukovića");
         }
     }
 }
